486658900d60398ec80df6823788ee5d477f140b
Dan Ringenbach <danringenbach.art@gmail.com>
2021-04-11 16:18:55 -0400

    final commit updates readme and script.js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 06-Weather-Dashboard
+## Description
+The task of this project was to create a simple weather dashboard that would display the specific time, temperature, wind speed, humidity, and uv index of the user's desired city.  Also displayed is the 5 day forecast of the selected city including temperature, humidity, and an icon describing upcoming weather conditions.  Weather data was acalled from the Open Weather API and current time was calculated by Moment JS. 
+
+**Links**
+
+* [Deployed Webpage](https://danringenbach.github.io/05-Work-Day-Scheduler/ "Deployed Webpage")
+* [Repository Link](https://github.com/DanRingenbach/05-Work-Day-Scheduler "Repository Link")
+
+
+**Screenshot**
+ 
+ ![alt text](screenshot.png)
+ 
+ 
--- script.js
+++ script.js
@@ -3,8 +3,19 @@ currentTime = $('#current-time').text(moment().format('L'))
 var weatherIcon = 'http://openweathermap.org/img/wn/'
 userInput = $('#user-input').val()
 var cityName = userInput
+var lastSearch = (localStorage.getItem("city"));
 
-$('#select-city').click(function (event) {
+$(document).ready(function pageLoad() {
+
+  var lastSearch = (localStorage.getItem("city"));
+  var searchDiv = $("<button id='previous-search class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(lastSearch);
+  var psearch = $("<div>");
+  psearch.append(searchDiv)
+  $("#search-nav").append(psearch);
+
+});
+
+$('#select-city').click(function weatherCall(event) {
   event.preventDefault();
   
   $('#current-weather').empty()
@@ -18,29 +29,30 @@ $('#select-city').click(function (event) {
       return response.json()
     })
     .then(function (data) {
-    
+
       var name = $('<h3>').text(data.name)
       $('#current-weather').append(name)
-      
+
       var date = $('<h3>').text(moment.unix(data.dt).format('MM/DD/yyyy'))
       date.val('')
       $('#current-weather').append(date)
-      
+
       var icon = $('<img>').attr('src', weatherIcon + data.weather[0].icon + '.png')
       var icon = icon.attr("style", "height: 40px; width: 40px")
       $('#current-weather').append(icon)
-      
+
       var temp = $('<h3>').text("Temperature: " + data.main.temp)
       $('#current-weather').append(temp)
-      
+
       var windSpeed = $('<h3>').text("Wind Speed: " + data.wind.speed)
       $('#current-weather').append(windSpeed)
-      
+
       var humid = $('<h3>').text("Humidity: " + data.main.humidity)
       $('#current-weather').append(humid)
 
       getUvi(data.coord.lat, data.coord.lon);
 
+      localStorage.setItem("city", searchCity)
 
     })
 
@@ -49,7 +61,6 @@ $('#select-city').click(function (event) {
 });
 
 function getUvi(lat, lon) {
-  console.log(lat, lon)
   var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=95d015b55c7f192344f9c89c2f0a415b'
 
   fetch(oneCall)
@@ -63,20 +74,20 @@ function getUvi(lat, lon) {
 
       // five-day-forecast
       for (var i = 0; i < 5; i++) {
-        
+
         var temp = data.daily[i].temp.day
         var humid = data.daily[i].humidity
         var icon = data.daily[i].weather[0].icon
         var unixTime = data.daily[i].dt;
         var date = new Date(unixTime * 1000);
         var date = (date.toLocaleDateString("en-US"));
-        
+
         var dailyDiv = $("<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2 col-3 col-md-2' style='width: 8.5rem; height: 15rem;'>");
         var h5date = $("<h4 class='card-title'>").text(date);
         var pTemp = $("<p class='card-text'>").text("Temp: " + temp);
         var pHum = $("<p class='card-text'>").text("Humidity: " + humid);
         var imgIcon = $('<img>').attr('src', weatherIcon + icon + '.png')
-        var imgIcon = imgIcon.attr('style',"height: 40px; width: 40px")
+        var imgIcon = imgIcon.attr('style', "height: 40px; width: 40px")
 
         dailyDiv.append(h5date);
         dailyDiv.append(pTemp);
@@ -84,7 +95,7 @@ function getUvi(lat, lon) {
         dailyDiv.append(imgIcon);
         $("#fiveDay").append(dailyDiv);
 
-        
+
 
 
       }
