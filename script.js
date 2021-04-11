2c6954a8e00d834ced243388800e688f3ddf994c
Dan Ringenbach <danringenbach.art@gmail.com>
2021-04-11 13:57:01 -0400

    update to dashboard functionality

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 submitBtn = $('#select-city')
 currentTime = $('#current-time').text(moment().format('L'))
 var weatherIcon = 'http://openweathermap.org/img/wn/'
+userInput = $('#user-input').val()
+var cityName = userInput
 
 $('#select-city').click(function (event) {
   event.preventDefault();
-  console.log('working')
+  
+  $('#current-weather').empty()
+  $('#fiveDay').empty()
+
   var searchCity = $('#user-input').val()
   var dayUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&units=imperial&appid=95d015b55c7f192344f9c89c2f0a415b';
 
@@ -13,27 +18,26 @@ $('#select-city').click(function (event) {
       return response.json()
     })
     .then(function (data) {
-      console.log(data)
-      console.log(data.name)
-      console.log(data.dt)
-      console.log(data.weather[0].icon)
-      console.log(data.main.temp)
-      console.log(data.wind.speed)
-      console.log(data.main.humidity)
+    
       var name = $('<h3>').text(data.name)
       $('#current-weather').append(name)
-      console.log(name)
+      
       var date = $('<h3>').text(moment.unix(data.dt).format('MM/DD/yyyy'))
+      date.val('')
       $('#current-weather').append(date)
+      
       var icon = $('<img>').attr('src', weatherIcon + data.weather[0].icon + '.png')
+      var icon = icon.attr("style", "height: 40px; width: 40px")
       $('#current-weather').append(icon)
-      var temp = $('<h3>').text(data.temp)
+      
+      var temp = $('<h3>').text("Temperature: " + data.main.temp)
       $('#current-weather').append(temp)
-      var windSpeed = $('<h3>').text(data.wind.speed)
+      
+      var windSpeed = $('<h3>').text("Wind Speed: " + data.wind.speed)
       $('#current-weather').append(windSpeed)
-      var humid = $('<h3>').text(data.main.humidity)
+      
+      var humid = $('<h3>').text("Humidity: " + data.main.humidity)
       $('#current-weather').append(humid)
-
 
       getUvi(data.coord.lat, data.coord.lon);
 
@@ -54,18 +58,47 @@ function getUvi(lat, lon) {
     })
     .then(function (data) {
       console.log(data)
-      var uvi = $('<h3>').text(data.current.uvi)
+      var uvi = $('<h3>').text("UV Index: " + data.current.uvi)
       $('#current-weather').append(uvi)
+
+      // five-day-forecast
+      for (var i = 0; i < 5; i++) {
+        
+        var temp = data.daily[i].temp.day
+        var humid = data.daily[i].humidity
+        var icon = data.daily[i].weather[0].icon
+        var unixTime = data.daily[i].dt;
+        var date = new Date(unixTime * 1000);
+        var date = (date.toLocaleDateString("en-US"));
+        
+        var dailyDiv = $("<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2 col-3 col-md-2' style='width: 8.5rem; height: 15rem;'>");
+        var h5date = $("<h4 class='card-title'>").text(date);
+        var pTemp = $("<p class='card-text'>").text("Temp: " + temp);
+        var pHum = $("<p class='card-text'>").text("Humidity: " + humid);
+        var imgIcon = $('<img>').attr('src', weatherIcon + icon + '.png')
+        var imgIcon = imgIcon.attr('style',"height: 40px; width: 40px")
+
+        dailyDiv.append(h5date);
+        dailyDiv.append(pTemp);
+        dailyDiv.append(pHum);
+        dailyDiv.append(imgIcon);
+        $("#fiveDay").append(dailyDiv);
+
+        
+
+
+      }
     })
 }
 
 
 
 
-// submitBtn.click(getApi());
 
-userInput = $('#user-input').val()
 
-var cityName = userInput
+
+
+
+
 
 
